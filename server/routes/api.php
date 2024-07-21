fa8bedaa96dfe78102779f3dbd7e26240928ddae
anggara.setiawan <aanggaadc@gmail.com>
2024-07-21 18:16:48 +0700

    refactor api routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Api\TodoController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::get('/users', [UserController::class, 'index'])->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/todos', TodoController::class);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
